13ff2916e3e5eb4176bc421070c1919d2d6a512e
alvaro <alpalencia01@gmail.com>
2025-08-09 18:38:23 +0200

    Adaptacion JSON

--- src/llmtrace/__main__.py
+++ src/llmtrace/__main__.py
@@ -1,43 +1,38 @@
 import os
-
 import click
 
 from dataset.dataset import Dataset
-from lib.clean_raw_data import clean_raw_data
+from lib.clean_new_data import clean_new_data
 from llm_eval.llm_eval import LLM_Evaluator
 from scraper.scraper import Scraper
 
-
 @click.group()
-def wireshairk():
+def llmtrace():
     click.echo("--------------------")
-    click.echo("     Wireshairk")
+    click.echo("     LlmTrace")
     click.echo("--------------------")
 
-
-@wireshairk.command()
+@llmtrace.command()
 def scrape():
     click.echo("Scraping")
     scraper = Scraper()
     scraper.download_captures()
 
-
-@wireshairk.command()
+@llmtrace.command()
 def clean_raw():
     click.echo("Cleaning raw data")
     clean_raw_data()
 
-
-@wireshairk.command()
+@llmtrace.command()
 def scrape_and_clean():
     click.echo("Scraping and cleaning raw data")
-    scraper = Scraper()
-    scraper.download_captures()
+    #scraper = Scraper()
+    #scraper.download_captures()
     clean_raw_data()
+    #clean_new_data()
 
-
-@wireshairk.command()
-@click.option("--data_path", default="data/cleaned/", help="Path to the data folder")
+@llmtrace.command()
+@click.option("--data_path", default="data/cleaned", help="Path to the data folder")
 @click.option(
     "--zero_shot",
     is_flag=True,
@@ -57,7 +52,92 @@ def scrape_and_clean():
 def generate_dataset(data_path, zero_shot, one_shot, chain_of_thought):
     click.echo("Generating dataset")
     dataset = Dataset(os.path.join(os.getcwd(), data_path))
-    capture_example = "No.|Time|Source|Destination|Protocol|Length|Port src.|Port dst.|Info\n 1 | 0.000000 | 145.254.160.237 | 65.208.228.223 | TCP | 62 | 3372 -> 80 [SYN] Seq=0 Win=8760 Len=0 MSS=1460 SACK_PERM\n2 | 0.911310 | 65.208.228.223 | 145.254.160.237 | TCP | 62 | 80 -> 3372 [SYN, ACK] Seq=0 Ack=1 Win=5840 Len=0 MSS=1380 SACK_PERM\n3 | 0.911310 | 145.254.160.237 | 65.208.228.223 | TCP | 54 | 3372 -> 80 [ACK] Seq=1 Ack=1 Win=9660 Len=0\n4 | 0.911310 | 145.254.160.237 | 65.208.228.223 | HTTP | 533 | GET /download.html HTTP/1.1\n5 | 1.472116 | 145.254.160.237 | 145.253.2.203 | DNS | 89 Standard query 0x0023 A pagead2.googlesyndication.com"
+    capture_example = '''[
+        {
+            "sid": "527",
+            "generator_id": "1",
+            "revision": "8",
+            "message": "BAD-TRAFFIC same SRC/DST",
+            "classification": "Potentially Bad Traffic",
+            "priority": 2,
+            "timestamp": "05/30-19:09:10.917356",
+            "source_ip": "0.0.0.0",
+            "source_port": 68,
+            "destination_ip": "255.255.255.255",
+            "destination_port": 67,
+            "protocol": "UDP",
+            "ttl": 128,
+            "tos": "0x0",
+            "id": 0,
+            "ip_length": 20,
+            "datagram_length": 328,
+            "payload_length": 300,
+            "references": []
+        },
+        {
+            "sid": "527",
+            "generator_id": "1",
+            "revision": "8",
+            "message": "BAD-TRAFFIC same SRC/DST",
+            "classification": "Potentially Bad Traffic",
+            "priority": 2,
+            "timestamp": "05/30-19:09:10.918155",
+            "source_ip": "0.0.0.0",
+            "source_port": 68,
+            "destination_ip": "255.255.255.255",
+            "destination_port": 67,
+            "protocol": "UDP",
+            "ttl": 128,
+            "tos": "0x0",
+            "id": 1,
+            "ip_length": 20,
+            "datagram_length": 354,
+            "payload_length": 326,
+            "references": []
+        },
+        {
+            "sid": "2014895",
+            "generator_id": "1",
+            "revision": "5",
+            "message": "ET CURRENT_EVENTS RedKit - Landing Page Received",
+            "classification": "A Network Trojan was Detected",
+            "priority": 1,
+            "timestamp": "05/30-19:09:10.927356",
+            "source_ip": "188.72.248.160",
+            "source_port": 80,
+            "destination_ip": "192.168.88.10",
+            "destination_port": 80,
+            "protocol": "TCP",
+            "ttl": 54,
+            "tos": "0x0",
+            "id": 15975,
+            "ip_length": 20,
+            "datagram_length": 714,
+            "payload_length": null,
+            "references": ["http://cve.mitre.org/cgi-bin/cvename.cgi?name=2012-4681"]
+        },
+        {
+            "sid": "2012811",
+            "generator_id": "1",
+            "revision": "2",
+            "message": "ET DNS DNS Query to a .tk domain",
+            "classification": "Potentially Bad Traffic",
+            "priority": 2,
+            "timestamp": "05/30-19:09:11.917356",
+            "source_ip": "192.168.88.10",
+            "source_port": 1029,
+            "destination_ip": "4.2.2.3",
+            "destination_port": 53,
+            "protocol": "UDP",
+            "ttl": 128,
+            "tos": "0x0",
+            "id": 78,
+            "ip_length": 20,
+            "datagram_length": 59,
+            "payload_length": 31,
+            "references": []
+        }
+    ]'''
     questions = dataset.get_questions()
 
     if one_shot:
@@ -67,15 +147,15 @@ def generate_dataset(data_path, zero_shot, one_shot, chain_of_thought):
         dataset.generate_dataset(
             "one_shot",
             [
-                f"{capture_example}\nQ: {questions[0]}\nA: In the capture there are a total of 5 packets",
-                f"{capture_example}\nQ: {questions[1]}\nA: There are a total of 3 unique communicators in the trace. These are the IPs: 145.254.160.237, 65.208.228.223, 145.253.2.203",
-                f"{capture_example}\nQ: {questions[2]}\nA: The IP that participates the most in the communication is the IP 145.254.160.237 this appears in a total of 5 packets.",
-                f"{capture_example}\nQ: {questions[3]}\nA: The total size of transmitted bytes is 800.",
-                f"{capture_example}\nQ: {questions[4]}\nA: The average size of packets in bytes is 160 bytes.",
-                f"{capture_example}\nQ: {questions[5]}\nA: In the capture predominates the use of TCP.",
-                f"{capture_example}\nQ: {questions[6]}\nA: The communication lasts 1.472116 seconds.",
-                f"{capture_example}\nQ: {questions[7]}\nA: The average of packets sent per second is 3.39.",
-                f"{capture_example}\nQ: {questions[8]}\nA: The average bytes/s sent in the communication is 543.43 bytes per second.",
+                f"{capture_example}\nQ: {questions[0]}\nA: In the file there are a total of 4 alerts.",
+                f"{capture_example}\nQ: {questions[1]}\nA: The most frequent classification is Potentially Bad Traffic with 3 occurrences.",
+                f"{capture_example}\nQ: {questions[2]}\nA: Public IP addresses are more frequent as the destination in the alerts with 3 occurrences.",
+                f"{capture_example}\nQ: {questions[3]}\nA: The average datagram length is 363.75 bytes across 4 valid alerts.",
+                f"{capture_example}\nQ: {questions[4]}\nA: There are 1 alerts with high priority (priority 1).",
+                f"{capture_example}\nQ: {questions[5]}\nA: No source IPs with multiple classifications found in 1 minute.",
+                f"{capture_example}\nQ: {questions[6]}\nA: 1 alerts with CVE references targeting vulnerable ports: Alert SID 2014895 (ID 15975, port 80, message: ET CURRENT_EVENTS RedKit - Landing Page Received).",
+                f"{capture_example}\nQ: {questions[7]}\nA: Top destination ports: 67 (50.00%, 2 alerts), 80 (25.00%, 1 alerts), 53 (25.00%, 1 alerts).",
+                f"{capture_example}\nQ: {questions[8]}\nA: Source IPs with focused attack attempts: 0.0.0.0: 2 alerts targeting 255.255.255.255 in 0.00s.",
             ],
         )
     elif chain_of_thought:
@@ -85,15 +165,15 @@ def generate_dataset(data_path, zero_shot, one_shot, chain_of_thought):
         dataset.generate_dataset(
             "chain_of_thought",
             [
-                f"{capture_example}\nQ: {questions[0]}\nA: The last row of the capture No column is 5. So the total number of packets in the trace is 5",
-                f"{capture_example}\nQ: {questions[1]}\nA: In the first row, the Source and Destination columns show the IPs 145.254.160.237 and 65.208.228.223, this IPs never appear before so there are 2 unique communicators for now. In the second row, 65.208.228.223 and 145.254.160.237 appear again, so we don't add new communicators. In the row three and four the IPs 65.208.228.223 and 145.254.160.237 repeat again so we can not add new communicators. In the row five appears a new IP never see before, the IP 145.253.2.203. So there is not more rows, we can conclude that there are a total of 3 unique communicators in the trace. These are the IPs: 145.254.160.237, 65.208.228.223, 145.253.2.203",
-                f"{capture_example}\nQ: {questions[2]}\nA: Aparition IP counter: First row: 145.254.160.237: 1 occurrences, 65.208.228.223: 1 occurrence // Second row: 145.254.160.237: 2 occurrences, 65.208.228.223 2 occurrences // Third row: 145.254.160.237: 3 occurrences, 65.208.228.223 3 occurrences // Fourth row: 145.254.160.237: 4 occurrences, 65.208.228.223 4 occurrences // Fifth row: 145.254.160.237: 5 occurrences, 145.253.2.203 1 occurrence. So the IP that participates the most in the communication is the IP 145.254.160.237 because have the higger number of appearances in the trace: 5 appearances in total.",
-                f"{capture_example}\nQ: {questions[3]}\nA: To see the length of the packet I have checked the 6th column value. So adding up all the packages length we can obtain the result: first packet has a length of 62 bytes, second packet has a length of 62 bytes, third packet has a length of 54 bytes, fourth packet has a length of 533 bytes and fifth packet has a length of 89 bytes. In conclusion the total size of transmitted bytes is 62+62+54+533+89=800 bytes",
-                f"{capture_example}\nQ: {questions[4]}\nA: To see the length of the packet I have checked the 6th column value. So adding up all the packages length we can obtain the result: first packet has a length of 62 bytes, the second packet has a length of 62 bytes, the third packet has a length of 54 bytes, fourth packet has a length of 533 bytes and fifth packet has a length of 89 bytes. In conclusion the average size of packets in bytes is the total size bytes divided by the number of packets: (62+62+54+533+89)bytes/(5)packets=(160)bytes/packet.",
-                f"{capture_example}\nQ: {questions[5]}\nA: The first packet uses TCP protocol, second packet uses TCP protocol, third packet uses TCP protocol, fourth packet uses HTTP protocol and fifth packet uses DNS protocol. So in the capture predominates the use of TCP, because TCP has 3 occurences in the trace, what is more occurences than HTTP and DNS that only have 1 occurence each.",
-                f"{capture_example}\nQ: {questions[6]}\nA: The last packet has a time of 1.472116 seconds and the first packet has a time of 0.000000 seconds. So the communication lasts 1.472116-0.000000=1.472116 seconds.",
-                f"{capture_example}\nQ: {questions[7]}\nA: The last packet has a time of 1.472116 seconds and the first packet has a time of 0.000000 seconds. So the communication lasts 1.472116-0.000000=1.472116 seconds. The total number of packets in the trace is 5. So the average of packets sent per second is (5)packets/(1.472116)seconds=(3.39)packets per second.",
-                f"{capture_example}\nQ: {questions[8]}\nA: The last packet has a time of 1.472116 seconds and the first packet has a time of 0.000000 seconds. So the communication lasts 1.472116-0.000000=1.472116 seconds. The total size of transmitted bytes is 62+62+54+533+89=800 bytes. So the average bytes per second sent in the communication is (800)bytes/(1.472116)seconds=(543.43)bytes/second",
+                f"{capture_example}\nQ: {questions[0]}\nA: The JSON array contains 4 alert entries. Thus, the total number of alerts is 4.",
+                f"{capture_example}\nQ: {questions[1]}\nA: Classifications: Alert 1: Potentially Bad Traffic, Alert 2: Potentially Bad Traffic, Alert 3: A Network Trojan was Detected, Alert 4: Potentially Bad Traffic. Counts: Potentially Bad Traffic (3), A Network Trojan was Detected (1). The most annoying classification is Potentially Bad Traffic with 3 occurrences.",
+                f"{capture_example}\nQ: {questions[2]}\nA: Destination IPs: Alert 1: 255.255.255.255 (public), Alert 2: 255.255.255.255 (public), Alert 3: 192.168.88.10 (private), Alert 4: 4.2.2.3 (public). Public: 3, Private: 1. Public IP addresses are more frequent with 3 occurrences.",
+                f"{capture_example}\nQ: {questions[3]}\nA: Datagram lengths: Alert 1: 328 bytes, Alert 2: 354 bytes, Alert 3: 714 bytes, Alert 4: 59 bytes. Total = 328 + 354 + 714 + 59 = 1455 bytes. Average = 1455 / 4 = 363.75 bytes.",
+                f"{capture_example}\nQ: {questions[4]}\nA: Priorities: Alert 1: 2, Alert 2: 2, Alert 3: 1, Alert 4: 2. Only Alert 3 has priority 1. Thus, there are 1 alerts with high priority (priority 1).",
+                f"{capture_example}\nQ: {questions[5]}\nA: Source IPs: 0.0.0.0 (Potentially Bad Traffic, 2x), 188.72.248.160 (A Network Trojan), 192.168.88.10 (Potentially Bad Traffic). Timestamps are within ~1s (< 60s). No source IP has multiple classifications. Thus, no source IPs with multiple classifications found.",
+                f"{capture_example}\nQ: {questions[6]}\nA: Vulnerable ports: 80, 443, 445. Alert 1: port 67, no CVE. Alert 2: port 67, no CVE. Alert 3: port 80, has CVE-2012-4681. Alert 4: port 53, no CVE. One alert meets the criteria: SID 2014895 (ID 15975, port 80, message: ET CURRENT_EVENTS RedKit - Landing Page Received).",
+                f"{capture_example}\nQ: {questions[7]}\nA: Ports: Alert 1: 67, Alert 2: 67, Alert 3: 80, Alert 4: 53. Counts: 67 (2), 80 (1), 53 (1). Total alerts: 4. Percentages: 67 (50.00%), 80 (25.00%), 53 (25.00%). Thus, ports: 67 (50.00%, 2 alerts), 80 (25.00%, 1 alerts), 53 (25.00%, 1 alerts).",
+                f"{capture_example}\nQ: {questions[8]}\nA: Pairs: (0.0.0.0, 255.255.255.255): 2 alerts (timestamps 19:09:10.917356, 19:09:10.918155, Δt ≈ 0.000799s), (188.72.248.160, 192.168.88.10): 1 alert, (192.168.88.10, 4.2.2.3): 1 alert. The pair (0.0.0.0, 255.255.255.255) has 2 alerts within 30 seconds. Thus, 0.0.0.0: 2 alerts targeting 255.255.255.255 in 0.00s."
             ],
         )
     else:
@@ -104,9 +184,9 @@ def generate_dataset(data_path, zero_shot, one_shot, chain_of_thought):
             "zero_shot",
             10 * [""],
         )
+    
 
-
-@wireshairk.command()
+@llmtrace.command()
 @click.option("--model", default="llama2", help="Model to evaluate")
 @click.option(
     "--dataset_path", default="data/zero_shot/dataset.jsonl", help="Path to the dataset"
@@ -121,22 +201,24 @@ def answer_questions(model, dataset_path, output_path):
     evaluator = LLM_Evaluator(model_to_eval=model, dataset_path=dataset_path)
     evaluator.answer_questions(output_path=output_path)
 
-
-@wireshairk.command()
+@llmtrace.command()
 @click.option(
     "--generated_output",
     default="data/evaluation/generated_output_llama2.jsonl",
     help="Path to the generated output file",
 )
-def evaluate(generated_output):
+@click.option(
+    "--dataset_path",
+    default="data/zero_shot/dataset.jsonl",
+    help="Path to the dataset",
+)
+def evaluate(generated_output, dataset_path):
     click.echo("Generating report")
-    # Extract the name of evaluated model from the generated_output path
     model = generated_output.split("/")[-1].split("_")[-1].split(".")[0]
-    evaluator = LLM_Evaluator(model_to_eval=model)
+    evaluator = LLM_Evaluator(model_to_eval=model, dataset_path=dataset_path)
     evaluator.evaluate_model(generated_output_path=generated_output)
 
-
-@wireshairk.command()
+@llmtrace.command()
 @click.option(
     "--evaluation_input", default="data/evaluation/generated_output_llama2.jsonl"
 )
@@ -146,6 +228,5 @@ def generate_report(evaluation_input):
     evaluator = LLM_Evaluator(model_to_eval=model)
     evaluator.generate_report(evaluation_input)
 
-
 if __name__ == "__main__":
-    wireshairk()
+    llmtrace()
